30d96427b373b7e9e3312aaa38f753302e3fb6d9
saravanan <s.k.saravanan94@gmail.com>
2019-04-21 14:20:51 +0530

    gender classification using decisiontree

--- twitter_sentimentalanalysi.py
+++ twitter_sentimentalanalysi.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = "eVzpDSlfBp29JWMPKzDgp0x4b"
-consumer_secret = "5lGpNQIcni5VhMhZyfsZIVUkvrF5aqYXFslahRHBeBZlqNM1Cf"
+consumer_key = ""
+consumer_secret = ""
 
-access_token = "2837991853-A0rnt8Il8qZPnBxxGWLLe5QBAlmPaeEDaHuEsFc"
-access_token_secret = "KRsBwc6Ob9wCpKh95nQycZCgYP6egoooOqwEKuch9Qr2l"
+access_token = ""
+access_token_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
